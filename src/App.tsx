26211942281d791c0e890f6e41c64d54b26f15ab
Layla Ventillari <laylaventilari5@gmail.com>
2022-10-07 20:11:35 -0300

    styles

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyle } from './styles/global'
+import './App.css'
+
+export function App() {
+
+
+  return (
+    <div className="App">
+      <h1>Coffe Delivery</h1>
+    </div>
+  )
+}
+
+
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+
+export const defaultTheme = {
+  white: '#FFFFFF',
+
+  'background-base': '#FAFAFA',
+  'base-card': '#F3F2F2',
+  'base-input': '#EDEDED',
+  'base-button': '#E6E5E5',
+  'base-hover': '#8D8D99',
+  'base-label': '#8D8686',
+  'base-text': '#574f4D',
+  'base-subtitle': '#403937',
+  'base-title': '#272221',
+
+  'purple-light': '#EBE5F9',
+  'purple': '#8047F8',
+  'purple-dark': '#4B2995',
+
+  'yellow-light': '#F1E9C9',
+  'yellow': '#DBAC2C',
+  'yellow-dark': '#C47F17',
+}
