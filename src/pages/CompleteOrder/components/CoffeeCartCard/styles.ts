e04f6dfe7859703279161442d9b8ad37c55c7cac
Layla Ventillari <laylaventilari5@gmail.com>
2022-12-17 15:48:28 -0300

    form

---
+++ src/pages/CompleteOrder/components/CoffeeCartCard/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const CoffeeCartCardContainer = styled.div`
+width: 100%;
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+> div {
+  img{
+    width: 4rem;
+    height: 4rem;
+  }
+}
+`;
+
+export const ActionsContainer = styled.div``;
+
+export const RemoveButton = styled.button``;
---
+++ src/pages/CompleteOrder/components/SelectedCoffees/index.tsx
@@ -0,0 +1,18 @@
+import { TitleText } from "../../../../components/Typography";
+import { CoffeCartCard } from "../CoffeeCartCard";
+import { DetailsContainer, SelectedCoffeesContainer } from "./styles";
+
+export function SelectedCoffees(){
+  return(
+    <SelectedCoffeesContainer>
+      <TitleText size="xs" color="subtitle">
+      Cafés selecionados
+      </TitleText>
+      <DetailsContainer>
+        <CoffeCartCard />
+        <CoffeCartCard />
+        <CoffeCartCard />
+      </DetailsContainer>
+    </SelectedCoffeesContainer>
+  )
+}
