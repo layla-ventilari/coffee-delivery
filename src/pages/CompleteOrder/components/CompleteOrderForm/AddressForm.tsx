1d4160251aa555a1bbca47629ccb1a18076eb0c0
Layla Ventillari <laylaventilari5@gmail.com>
2022-12-27 22:09:14 -0300

    useCart

--- src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
@@ -1,10 +1,19 @@
+import { useFormContext } from "react-hook-form";
 import { Input } from "../../../../components/Input/input";
 import { AddressFormContainer } from "./styles";
 
 export function AddressForm(){
+
+  const {register} = useFormContext();
+
   return(
     <AddressFormContainer>
-      <Input placeholder="CEP" type="number" className="cep"/>
+      <Input 
+        placeholder="CEP" 
+        type="number" 
+        className="cep"
+        {...register("cep")}
+        />
       <Input placeholder="Rua" className="street"  />
       <Input placeholder="Número" className="number" />
       <Input placeholder="Complemento" className="complement" />
